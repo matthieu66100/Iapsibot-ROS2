21fff50a6ab698d4a819025831590e9fa2aa3edb
benjamin <benjamin.carpene@apside.com>
2023-09-22 09:57:40 +0200

    Premier commit

---
+++ src/iapsibot_tests_pkg/src/test_com_publisher.cpp
@@ -0,0 +1,46 @@
+#include <chrono>
+#include <memory>
+#include <functional>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/range.hpp"
+
+using namespace std::chrono_literals;
+
+/**
+ * @brief Teste la réception d'image, l'etat du robot et publie les commandes vers le robot
+ * 
+ */
+class TestComPublisher : public rclcpp::Node
+{
+public:
+  TestComPublisher()
+  : Node("test_com_publisher"), mCount(0)
+  {
+    mPublisher = this->create_publisher<std_msgs::msg::String>("stringTopic", 10);
+    mTimer = this->create_wall_timer(
+      500ms, std::bind(&TestComPublisher::timerCallbackString, this));
+    
+  }
+
+private:
+  void timerCallbackString()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Bonjour le monde " + std::to_string(mCount++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    mPublisher->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr mTimer;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr mPublisher;
+  size_t mCount;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TestComPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
---
+++ src/iapsibot_tests_pkg/src/test_com_suscriber.cpp
@@ -0,0 +1,40 @@
+#include "rclcpp/rclcpp.hpp"
+
+#include <memory>
+#include <functional>
+#include "std_msgs/msg/string.hpp"
+
+
+using std::placeholders::_1;
+
+class TestComSuscriber : public rclcpp::Node
+{
+public:
+    TestComSuscriber(): Node("test_com_suscriber")
+    {
+        //String topic
+
+        mSuscription = this->create_subscription<std_msgs::msg::String>("stringTopic", 10, 
+            std::bind(&TestComSuscriber::callbackFunctionString, this, _1)
+        );
+    }
+private:
+
+    void callbackFunctionString(std::shared_ptr<const std_msgs::msg::String> msg){
+
+        std::stringstream printedMsg;
+        printedMsg << "I heard on topic 'stringTopic : ' " << msg->data; 
+        RCLCPP_INFO(this->get_logger(), printedMsg.str().c_str());
+    }
+
+    std::shared_ptr<rclcpp::Subscription<std_msgs::msg::String>> mSuscription; 
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<TestComSuscriber>());
+  rclcpp::shutdown();
+
+  return 0;
+}
