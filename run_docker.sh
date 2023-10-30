e3cd4bf7128187ee6405d9eee17051aa2c9e510e
benjamin <benjamin.carpene@gmail.com>
2023-10-30 17:26:34 +0100

    [DEV] : Messages

--- run_docker.sh
+++ run_docker.sh
@@ -3,9 +3,9 @@
 WEBOT_FILE="./dependencies/webots_2023b_amd64.deb"
 
 if [ -f "$WEBOT_FILE" ]; then
-    echo "LE FICHIER EST DEJA EN PLACE"
+    echo "[INFO] : LE FICHIER EST DEJA EN PLACE"
 else
-    echo "TELECHARGEMENT DU FICHIER WEBOTS..."
+    echo "[INFO] : FICHIER NON EXISTANT, TELECHARGEMENT DE WEBOTS..."
     cd ./dependencies
     wget https://github.com/cyberbotics/webots/releases/download/R2023b/webots_2023b_amd64.deb
 fi
